0f36e3f1050d4f81c7004a2604f4ef7fdbb29c5b
Lucas-Silva2601 <marciliojesusss@gmail.com>
2026-08-04 21:47:40 -0300

    Update servidor.example.php

--- app/config/servidor.example.php
+++ app/config/servidor.example.php
@@ -5,12 +5,13 @@ return array(
     'app_base_path' => '/estrategia',
 
     'db_driver' => 'pdo_sqlsrv',
-    'db_host' => 'SERVIDOR_SQL',
-    'db_database' => 'BANCO_ESTRATEGIA',
+    'db_host' => 'DF7436SR439',
+    'db_database' => 'DB5319_IndicadoresEstrategicos',
     'db_auth_mode' => 'sql',
-    'db_username' => '',
-    'db_password' => '',
+    'db_username' => 's531902',
+    'db_password' => 'Gelot02',
 
     'auth_provider' => 'legacy_file',
     'ldap_legacy_path' => 'C:/caminho/acessoldap/LDAP.php',
 );
+
